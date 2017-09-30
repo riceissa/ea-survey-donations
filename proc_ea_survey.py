2c2b625b0f0cd7efe0564ea0455341ab6c207a82
Issa Rice <riceissa@gmail.com>
2017-09-30 13:30:38 -0700

    Add currency fields

--- proc_ea_survey.py
+++ proc_ea_survey.py
@@ -83,7 +83,8 @@ def mysql_quote(x):
 print("""insert into donations(donor, donee, amount, fraction, donation_date,
     donation_date_precision, donation_date_basis, cause_area, url, notes,
     payment_modality, match_eligible, goal_amount, influencer, employer_match,
-    matching_employer) values""")
+    matching_employer, amount_original_currency, original_currency,
+    currency_conversion_date, currency_conversion_basis) values""")
 
 with open("2017-ea-survey-sharable-data.csv", newline='') as f:
     reader = csv.DictReader(f)
@@ -130,6 +131,10 @@ with open("2017-ea-survey-sharable-data.csv", newline='') as f:
                         "NULL",
                         "NULL",
                         "NULL",
+                        str(float(row[donation_var[:-len("_c")]])),  # amount original
+                        mysql_quote(row['currency']),  # original currency
+                        mysql_quote("2017-08-05"),
+                        mysql_quote("Peter Hurford"),
                     ]) + ")")
                     first = False
 
